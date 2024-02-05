196af03ac552cdb428048f3d212c780093da3645
Timothy Ryon <tim@timryon.com>
2024-02-05 11:32:10 -0500

    working add NewQuestion to DOM and db.json; not pass test

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -3,19 +3,49 @@ import React, { useState } from "react";
 function QuestionForm({ baseUrl, onHandleSubmit }) {
   const [formData, setFormData] = useState({
     prompt: "",
-    answer1: "",
-    answer2: "",
-    answer3: "",
-    answer4: "",
+    answers: [
+       "",
+       "",
+       "",
+       "",
+    ],
     correctIndex: 0,
   });
+  console.log(formData);
+  console.log(formData.answers[0]);
+
+  // const [formData, setFormData] = useState({
+  //   prompt: "",
+  //   answer1: "",
+  //   answer2: "",
+  //   answer3: "",
+  //   answer4: "",
+  //   correctIndex: 0,
+  // });
 
   function handleChange(event) {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
+    const { name, value } = event.target;
+
+    if(name.startsWith("answer")) {
+      const index =parseInt(name.charAt(name.length - 1), 10) -1;
+      setFormData({
+        ...formData,
+        answers: formData.answers.map((ans, i) => (i === index ? value : ans)),
+      });
+    } else {
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
+    }
   }
+
+  // function handleChange(event) {
+  //   setFormData({
+  //     ...formData,
+  //     [event.target.name]: event.target.value,
+  //   });
+  // }
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -26,10 +56,21 @@ function QuestionForm({ baseUrl, onHandleSubmit }) {
         "Content-Type" : "application/json",
       },
       // below body: works to update db.json, but browser errors as the answers are not in an array. change to {"answers": ["string", "string", "string","string"]  }??
-      body: JSON.stringify(formData, {
-        "prompt": "string",
-        "answers": "array of strings",
-        "correctIndex": "integer"
+      body: JSON.stringify({
+        prompt: formData.prompt,
+        answers: formData.answers,
+        correctIndex: formData.correctIndex,
+        // "prompt": "string",
+        // "answers": [
+        //   "string",
+        //   "string",
+        //   "string",
+        //   "string",
+        // ],
+        // "correctIndex": "integer"
+        // "prompt": "string",
+        // "answers": "array of strings",
+        // "correctIndex": "integer"
       })
     })
       .then((r) => r.json())
@@ -54,7 +95,7 @@ function QuestionForm({ baseUrl, onHandleSubmit }) {
           <input
             type="text"
             name="answer1"
-            value={formData.answer1}
+            value={formData.answers[0]}
             onChange={handleChange}
           />
         </label>
@@ -63,7 +104,7 @@ function QuestionForm({ baseUrl, onHandleSubmit }) {
           <input
             type="text"
             name="answer2"
-            value={formData.answer2}
+            value={formData.answers[1]}
             onChange={handleChange}
           />
         </label>
@@ -72,7 +113,7 @@ function QuestionForm({ baseUrl, onHandleSubmit }) {
           <input
             type="text"
             name="answer3"
-            value={formData.answer3}
+            value={formData.answers[2]}
             onChange={handleChange}
           />
         </label>
@@ -81,7 +122,7 @@ function QuestionForm({ baseUrl, onHandleSubmit }) {
           <input
             type="text"
             name="answer4"
-            value={formData.answer4}
+            value={formData.answers[3]}
             onChange={handleChange}
           />
         </label>
@@ -92,10 +133,14 @@ function QuestionForm({ baseUrl, onHandleSubmit }) {
             value={formData.correctIndex}
             onChange={handleChange}
           >
-            <option value="0">{formData.answer1}</option>
+            <option value="0">{formData.answers[0]}</option>
+            <option value="1">{formData.answers[1]}</option>
+            <option value="2">{formData.answers[2]}</option>
+            <option value="3">{formData.answers[3]}</option>
+            {/* <option value="0">{formData.answer1}</option>
             <option value="1">{formData.answer2}</option>
             <option value="2">{formData.answer3}</option>
-            <option value="3">{formData.answer4}</option>
+            <option value="3">{formData.answer4}</option> */}
           </select>
         </label>
         <button type="submit">Add Question</button>
